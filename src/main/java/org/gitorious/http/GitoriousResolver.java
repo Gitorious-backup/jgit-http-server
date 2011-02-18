bcb584625f0e4f358153385d31fb67af59a4a9ad
Marius Mathiesen <marius@shortcut.no>
2011-02-18 08:06:12 +0100

    Adding license & copyright

--- src/main/java/org/gitorious/http/GitoriousResolver.java
+++ src/main/java/org/gitorious/http/GitoriousResolver.java
@@ -1,8 +1,20 @@
 /*
- * This is a class that implements looking up Gitorious paths from 
- * the database.
- * Currently it's quite error prone, but works. Kind of
+ *   Copyright (C) 2011 Gitorious AS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.gitorious.http;
 
 import org.eclipse.jgit.http.server.resolver.*;
@@ -26,6 +38,11 @@ import org.apache.log4j.BasicConfigurator;
 import java.util.HashMap;
 
 
+/*
+ * This is a class that implements looking up Gitorious paths from 
+ * the database.
+ * Currently it's quite error prone, but works. Kind of
+ */
 class GitoriousResolver implements RepositoryResolver {
     private String repositoryRoot;
     private String permissionBaseUri;
--- src/main/java/org/gitorious/http/GitoriousServlet.java
+++ src/main/java/org/gitorious/http/GitoriousServlet.java
@@ -1,6 +1,20 @@
 /*
- * Extending JGit's GitServlet to use a custom repository resolver 
+ *   Copyright (C) 2011 Gitorious AS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.gitorious.http;
 import org.eclipse.jgit.http.server.*;
 import javax.servlet.ServletException;
@@ -9,6 +23,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.ReceivePack;
 
+/*
+ * Extending JGit's GitServlet to use a custom repository resolver 
+ */
 public class GitoriousServlet extends GitServlet {
     public GitoriousServlet() {
     }
